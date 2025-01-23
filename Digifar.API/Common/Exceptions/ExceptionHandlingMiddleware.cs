5c8dcd44dba0ae30bfb278b4caebdaa380d6e1c1
elikofi <elijahamoako72@gmail.com>
2025-01-23 09:29:52 +0000

    Setting up the Results pattern for all responses and then the global exception handler for all exceptions.

---
+++ Digifar.API/Common/Exceptions/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Digifar.API.Common.Exceptions
+{
+    public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        private readonly RequestDelegate _next = next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger = logger;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ValidationException ex)
+            {
+                _logger.LogError(ex, "Validation exception occurred.");
+                await HandleValidationExceptionAsync(context, ex);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandled exception occurred.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleValidationExceptionAsync(HttpContext context, ValidationException exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+            var errors = exception.Errors.Select(e => new { e.PropertyName, e.ErrorMessage }).ToList();
+            var result = JsonConvert.SerializeObject(new { errors });
+
+            return context.Response.WriteAsync(result);
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var result = JsonConvert.SerializeObject(new { error = exception.Message });
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ Digifar.API/Models/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Digifar.API.Models.Entities
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public double Wallet { get; set; } = 0;
+    }
+}
